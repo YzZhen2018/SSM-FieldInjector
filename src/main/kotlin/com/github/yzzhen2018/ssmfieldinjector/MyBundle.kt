1cc52b3eb7ae556d06327e6f8105c84e104ca605
GitHub Action <action@github.com>
2023-04-15 11:16:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/yzzhen2018/ssmfieldinjector/MyBundle.kt
+++ src/main/kotlin/com/github/yzzhen2018/ssmfieldinjector/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yzzhen2018.ssmfieldinjector
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/yzzhen2018/ssmfieldinjector/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/yzzhen2018/ssmfieldinjector/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yzzhen2018.ssmfieldinjector.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
